6a92acc1f5e3af0673fcda78ccc42774fc874c00
Joseph Collicoat <jcollicoat@gmail.com>
2021-08-30 23:55:05 +1200

    Follow course up to API routes section

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
 
 const name = "Joseph Collicoat";
 export const siteTitle = "Next.js Sample Website";
@@ -44,7 +45,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src="/images/profile.jpeg"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
